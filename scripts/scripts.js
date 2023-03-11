a1b25273193cfcb9a34f5b805abf19007dfca48e
Jonathan Parras <jparras@email.com>
2023-03-10 17:13:27 -0800

    adds an array containing my courses

---
+++ scripts/scripts.js
@@ -0,0 +1,14 @@
+const courseList = [
+    {
+        code: "ACIT 1620", 
+        name: "Fundamental Web Technologies"
+    },
+    {
+        code: "ACIT 1630",
+        name: "Database Systems"
+    },
+    {
+        code: "ACIT 1420",
+        name: "Introduction to Systems Administration"
+    }
+];
